922120fe1b1821ea3c67c3f446004f0d73f3d89d
Say.li <120011676@qq.com>
2019-10-25 11:49:10 +0800

    增加异常信息提示配置信息

--- ladybird-spring-boot-web-helper/src/main/java/com/github/qq120011676/ladybird/web/exception/autoconfigure/RestfulExceptionProperties.java
+++ ladybird-spring-boot-web-helper/src/main/java/com/github/qq120011676/ladybird/web/exception/autoconfigure/RestfulExceptionProperties.java
@@ -8,15 +8,41 @@ import java.util.Map;
 @Data
 @ConfigurationProperties(prefix = "ladybird.restful-exception")
 public class RestfulExceptionProperties {
+    /**
+     * 返回code字段名称
+     */
     private String codeName = "code";
+
+    /**
+     * 返回message字段名称
+     */
     private String messageName = "message";
+
+    /**
+     * 默认http状态值
+     */
     private int defaultHttpStatus = 400;
+
+    /**
+     * 默认code值
+     */
     private String defaultCode = "unknown";
+
+    /**
+     * 配置异常信息
+     */
     private Map<String, Message> messages;
 
     @Data
     public static class Message {
+        /**
+         * 异常信息返回的http状态（可选）
+         */
         private Integer httpStatus;
+
+        /**
+         * 异常信息返回的message信息（必须）
+         */
         private String message;
     }
 }
